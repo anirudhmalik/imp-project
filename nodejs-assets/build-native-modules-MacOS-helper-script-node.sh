b49b142bad13477d2c16537bdb6773abd703fb77
anirudhmalik <sfsanirudh@gmail.com>
2021-06-10 21:05:33 +0530

    "project structure"

---
+++ nodejs-assets/build-native-modules-MacOS-helper-script-node.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+      # Helper script for Gradle to call node on macOS in case it is not found
+      export PATH=$PATH:/Users/anirudh/Desktop/projects/test/test1/node_modules/nodejs-mobile-react-native/node_modules/.bin:/Users/anirudh/Desktop/projects/test/test1/node_modules/node_modules/.bin:/Users/anirudh/Desktop/projects/test/test1/node_modules/.bin:/Users/anirudh/Desktop/projects/test/node_modules/.bin:/Users/anirudh/Desktop/projects/node_modules/.bin:/Users/anirudh/Desktop/node_modules/.bin:/Users/anirudh/node_modules/.bin:/Users/node_modules/.bin:/node_modules/.bin:/usr/local/lib/node_modules/npm/node_modules/@npmcli/run-script/lib/node-gyp-bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin
+      node $@
+    
---
+++ nodejs-assets/nodejs-project/main.js
@@ -0,0 +1,93 @@
+var rn_bridge = require('rn-bridge');
+const test=()=>{
+
+
+/*const port = 8080;
+const server = http.createServer((req, res) =>{
+    res.end('Hello, World!');
+});
+
+server.listen(port, (err) => {
+    if (err) return console.log(`Something bad happened: ${err}`);
+    console.log(`Node.js server listening on ${port}`);
+   (async () => {
+   const tunnel = await localtunnel({ port: 8080,subdomain:"anirudh" });
+   rn_bridge.channel.send(
+    "sha3 output:\n" +
+    tunnel.url
+  );
+})();
+
+});
+*/
+const localtunnel = require('localtunnel');
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+
+const port = 8080;
+
+
+http.listen(port, (err) => {
+    if (err) return console.log(`Something bad happened: ${err}`);
+    console.log(`Node.js server listening on ${port}`);
+   (async () => {
+   const tunnel = await localtunnel({ port: 8080,subdomain:"anirudh" });
+   rn_bridge.channel.send(
+    "sha3 output:\n" +
+    tunnel.url
+  );
+})();
+
+});
+
+
+/**/
+
+app.use(express.static(__dirname + '/public'))
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+// Socket 
+const io = require('socket.io')(http)
+
+io.on('connection', (socket) => {
+    console.log('Connected...')
+    rn_bridge.channel.send("londa connected");
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg)
+        rn_bridge.channel.send(msg);
+    })
+
+})
+}
+
+
+
+rn_bridge.channel.on('message', (msg) => {
+  try {
+    switch(msg) {
+      case 'versions':
+        rn_bridge.channel.send(
+          "Versions: " +
+          JSON.stringify(process.versions)
+        );
+        break;
+      case 'sha3':
+        test()
+        break;
+      default:
+        rn_bridge.channel.send(
+          "unknown request:\n" +
+          msg
+        );
+        break;
+    }
+  } catch (err)
+  {
+    rn_bridge.channel.send("Error: " + JSON.stringify(err) + " => " + err.stack );
+  }
+});
+
+// Inform react-native node is initialized.
+rn_bridge.channel.send("Node was initialized. Versions: " + JSON.stringify(process.versions));
